ae07954bd8829051d98fa9b0e1ae538fd7a80951
Massoud Hamad <massoudhamad@gmail.com>
2020-08-05 06:17:44 +0300

    CRUD Lecture

---
+++ crud-lecture/edit.php
@@ -0,0 +1,73 @@
+<?php
+require_once "pdo.php";
+session_start();
+
+if ( isset($_POST['name']) && isset($_POST['email'])
+     && isset($_POST['password']) && isset($_POST['user_id']) ) {
+
+    // Data validation
+    if ( strlen($_POST['name']) < 1 || strlen($_POST['password']) < 1) {
+        $_SESSION['error'] = 'Missing data';
+        header("Location: edit.php?user_id=".$_POST['user_id']);
+        return;
+    }
+
+    if ( strpos($_POST['email'],'@') === false ) {
+        $_SESSION['error'] = 'Bad data';
+        header("Location: edit.php?user_id=".$_POST['user_id']);
+        return;
+    }
+
+    $sql = "UPDATE users SET name = :name,
+            email = :email, password = :password
+            WHERE user_id = :user_id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+        ':name' => $_POST['name'],
+        ':email' => $_POST['email'],
+        ':password' => $_POST['password'],
+        ':user_id' => $_POST['user_id']));
+    $_SESSION['success'] = 'Record updated';
+    header( 'Location: index.php' ) ;
+    return;
+}
+
+// Guardian: Make sure that user_id is present
+if ( ! isset($_GET['user_id']) ) {
+  $_SESSION['error'] = "Missing user_id";
+  header('Location: index.php');
+  return;
+}
+
+$stmt = $pdo->prepare("SELECT * FROM users where user_id = :xyz");
+$stmt->execute(array(":xyz" => $_GET['user_id']));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+if ( $row === false ) {
+    $_SESSION['error'] = 'Bad value for user_id';
+    header( 'Location: index.php' ) ;
+    return;
+}
+
+// Flash pattern
+if ( isset($_SESSION['error']) ) {
+    echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+    unset($_SESSION['error']);
+}
+
+$n = htmlentities($row['name']);
+$e = htmlentities($row['email']);
+$p = htmlentities($row['password']);
+$user_id = $row['user_id'];
+?>
+<p>Edit User</p>
+<form method="post">
+<p>Name:
+<input type="text" name="name" value="<?= $n ?>"></p>
+<p>Email:
+<input type="text" name="email" value="<?= $e ?>"></p>
+<p>Password:
+<input type="text" name="password" value="<?= $p ?>"></p>
+<input type="hidden" name="user_id" value="<?= $user_id ?>">
+<p><input type="submit" value="Update"/>
+<a href="index.php">Cancel</a></p>
+</form>
