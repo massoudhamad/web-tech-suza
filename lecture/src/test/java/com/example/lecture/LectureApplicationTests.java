f1169be067bfd6d23db6cb8c776b4f0e949417db
massoudhamad <massoudhamad@gmail.com>
2022-04-27 14:25:08 +0300

    Updates

---
+++ lecture/src/test/java/com/example/lecture/LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
